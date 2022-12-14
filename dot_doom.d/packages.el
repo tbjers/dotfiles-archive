2b785a2e3a0c73f0de0dde7c0f86367f6bf9ed1e
Torgny Bjers <torgny@bjers.org>
2022-12-14 14:55:37 -0500

    add helm external
    - add toml-mode to doom emacs
    - update mode lines

--- dot_doom.d/packages.el
+++ dot_doom.d/packages.el
@@ -21,6 +21,7 @@
 (package! ranger)
 (package! go-mode)
 (package! company-go)
+(package! toml-mode)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
